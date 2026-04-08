da63978fe8b052c632c3363a56a30988ff0ae523
Michael Smith <throwawayclover@gmail.com>
2026-04-08 15:56:16 -0400

    chore: finish 07

--- src/problems/07-tuple-to-union-challenge-3/07-tuple-to-union-challenge-3.ts
+++ src/problems/07-tuple-to-union-challenge-3/07-tuple-to-union-challenge-3.ts
@@ -9,15 +9,15 @@
  * type Test = TupleToUnion<Arr> // '1' | '2' | '3'
  */
 
-import type { Equal, Expect } from '@course/types'
+import type { Equal, Expect } from "@course/types";
 
 /* _____________ Your Code Here _____________ */
 
-type TupleToUnion<T extends readonly any[]> = T[number]
+type TupleToUnion<T extends readonly unknown[]> = T[number];
 
 /* _____________ Test Cases _____________ */
 
 type cases = [
-  Expect<Equal<TupleToUnion<[123, '456', true]>, 123 | '456' | true>>,
-  Expect<Equal<TupleToUnion<[123]>, 123>>,
-]
+	Expect<Equal<TupleToUnion<[123, "456", true]>, 123 | "456" | true>>,
+	Expect<Equal<TupleToUnion<[123]>, 123>>,
+];
